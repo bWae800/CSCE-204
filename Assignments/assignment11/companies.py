ea2e4f70e006e89c4442842620529e5dc3ae0cb0
bWae800 <89473894+bWae800@users.noreply.github.com>
2021-09-24 15:18:50 -0400

    Add files via upload

---
+++ Assignments/assignment11/companies.py
@@ -0,0 +1,31 @@
+#Author Brenden Wade
+
+companies = ['Google','Facebook', 'Instagram', 'AVX', '3D Systems','Human Technologies', 'Palmetto', 'Starbcuks', 'NBA', 'MLB']
+wishlist = []
+
+
+while True:
+    command = input("(V)iew, (A)dd company to wish list, (R)emove company from wish list, (Q)uit: ").strip().lower()
+
+    if command == "v":
+        ans = input("View (A)ll companies or your (W)ish list? ").lower().strip()
+        if ans == "a":
+            for company in companies:
+                print(f"- {company}")
+        elif ans == "w":
+            for wish in wishlist:
+                print(f"- {wish} ")
+        else:
+            print("Invalid command.")
+    elif command == "a":
+        company1 = input("Enter company name: ")
+        wishlist.append(company1)
+    elif command == "r":
+        company = input("Enter company name: ")
+        wishlist.remove(company)
+    elif command == 'q':
+        break
+    else:
+        print("Invalid command")
+
+print("Goodbye.")
